56b61a68bb24881a142a18317d28d71787fd9f7e
paula <paula@LAPTOP-572UOOEL>
2020-04-17 09:43:40 +0100

    Eliminar un getter

--- EED_UT5/src/mipackage/Account.java
+++ EED_UT5/src/mipackage/Account.java
@@ -11,92 +11,70 @@ package mipackage;
  */
 public class Account {
 
-
-    public String getTitular() {
+	public String getTitular() {
 		return titular;
 	}
-
 
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
 
-
 	public String getCcc() {
 		return ccc;
 	}
-
 
 	public void setCcc(String ccc) {
 		this.ccc = ccc;
 	}
 
-	
-
 	public double getBalance() {
-		return balance;
+		return dineroCuenta;
 	}
-
 
 	public void setBalance(double balance) {
-		this.balance = balance;
+		this.dineroCuenta = balance;
 	}
-
 
 	public Account() {
 		super();
 	}
-	
+
 	public Account(double balance) {
 		super();
 		this.setBalance(balance);
 	}
 
-
-
-
-
 	// Atributos de la clase Account
-    private String titular;
-    private String ccc;
-    private double balance = 0;
+	private String titular;
+	private String ccc;
+	private double dineroCuenta = 0;
 
- 
-    // Constructor
-    public Account (String nom, String cuenta)
-    {
-        this.setTitular(nom);
-        this.setCcc(cuenta);
-    }
-    
- 
-    // Método que me devuelve el saldo disponible en cada momento
-     public double disponible()
-    {
-        return getBalance();
-    }
+	// Constructor
+	public Account(String nom, String cuenta) {
+		this.setTitular(nom);
+		this.setCcc(cuenta);
+	}
 
-    /* Método para ingresar cantidades en la cuenta. Modifica el saldo.
-       */
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        setBalance(getBalance() + cantidad);
-    }
+	// Método que me devuelve el saldo disponible en cada momento
 
+	/*
+	 * Método para ingresar cantidades en la cuenta. Modifica el saldo.
+	 */
+	public void ingresar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		setBalance(getBalance() + cantidad);
+	}
 
+	/*
+	 * Metodo para retirar cantidades en la cuenta. Modifica el saldo.
+	 */
+	public void retirar(double cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede retirar una cantidad negativa.");
+		if (disponible() < cantidad)
+			throw new Exception("No hay suficiente saldo para retirar la cantidad solicitada.");
+		setBalance(getBalance() - cantidad);
+	}
 
-    /* Metodo para retirar cantidades en la cuenta. Modifica el saldo.
-     */
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad < 0)
-            throw new Exception ("No se puede retirar una cantidad negativa.");
-        if (disponible()< cantidad)
-            throw new Exception ("No hay suficiente saldo para retirar la cantidad solicitada.");
-        setBalance(getBalance() - cantidad);
-    }
-
- 
 }
